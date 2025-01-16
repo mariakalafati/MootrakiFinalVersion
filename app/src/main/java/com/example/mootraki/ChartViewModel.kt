2a344a8a06148de6ba7e04626e2f6c3c1035ac4f
ArgySk <116523553+ArgySk@users.noreply.github.com>
2025-01-16 15:12:39 +0200

    Update ChartViewModel.kt

--- app/src/main/java/com/example/mootraki/ChartViewModel.kt
+++ app/src/main/java/com/example/mootraki/ChartViewModel.kt
@@ -5,15 +5,12 @@ import androidx.lifecycle.viewModelScope
 import com.example.mootraki.data.EntriesRepository
 import com.example.mootraki.data.Entry
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ChartViewModel(private val entriesRepository: EntriesRepository) : ViewModel() {
     private val _allEntries = MutableStateFlow<List<Entry>>(emptyList())
-    val allEntries: StateFlow<List<Entry>> get() = _allEntries.asStateFlow()
 
     init {
         fetchAllEntries()
@@ -27,24 +24,18 @@ class ChartViewModel(private val entriesRepository: EntriesRepository) : ViewMod
         }
     }
 
-    // Count moods for days 10-16 of January 2025 in order to have data when showing example
     fun countMoodsForLastWeek(): Map<Int, Int> {
         val entries = _allEntries.value
         val moodCount = mutableMapOf<Int, Int>()
         val dateFormat = SimpleDateFormat("yyyy-MM-dd")
 
-        // Define the date range
         val startDate = dateFormat.parse("2025-01-10")!!
         val endDate = dateFormat.parse("2025-01-16")!!
 
         entries.forEach { entry ->
             try {
-                // Parse the date from the entry
                 val date = dateFormat.parse(entry.date)!!
-
-                // Check if the date is within the specified range
                 if (date in startDate..endDate) {
-                    // Count the mood
                     moodCount[entry.mood] = moodCount.getOrDefault(entry.mood, 0) + 1
                 }
             } catch (e: Exception) {
@@ -54,7 +45,6 @@ class ChartViewModel(private val entriesRepository: EntriesRepository) : ViewMod
         return moodCount
     }
 
-    // Count occurrences of a specified mood for each month in 2024
     fun countMoodForEachMonth(mood: Int): Map<Int, Int> {
         val entries = _allEntries.value
         val moodCount = mutableMapOf<Int, Int>()
@@ -77,7 +67,6 @@ class ChartViewModel(private val entriesRepository: EntriesRepository) : ViewMod
         return moodCount
     }
 
-    // Count occurrences of a specified emotion for each month in 2024
     fun countEmotionForEachMonth(emotion: String): Map<Int, Int> {
         val entries = _allEntries.value
         val emotionCount = mutableMapOf<Int, Int>()
